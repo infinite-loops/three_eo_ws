ba239606ffa7e547333e163b64e78d3290dd5cab
Ivan Taranin <ivan.taranin@experienceone.com>
2021-02-25 10:51:12 +0100

    revert to clean

--- src/Main.js
+++ src/Main.js
@@ -9,7 +9,6 @@ import {
     AxesHelper, BackSide, BoxGeometry, Clock, DirectionalLight,
     GridHelper, Mesh, MeshBasicMaterial, PlaneBufferGeometry, SphereGeometry, TorusKnotGeometry,
 } from "three";
-import PortalsMain from "./portals/portals_main";
 
 
 
@@ -78,8 +77,6 @@ export default class GLStage extends Component{
             renderer, camera
         });
 
-        // essentials.scene.add(new AxesHelper(10));
-        // essentials.scene.add(new GridHelper(10, 10));
 
 
         this.renderer.setSize(this.width, this.height);
@@ -95,8 +92,6 @@ export default class GLStage extends Component{
             camera.updateProjectionMatrix();
         });
 
-        //TODO props object
-        this.Portals = initPortals({renderer: this.renderer, camera: this.camera});
 
         this.start();
     }
@@ -110,8 +105,6 @@ export default class GLStage extends Component{
 
     renderScene(){
         const dt = this.Clock.getDelta();
-        this.Portals.update(dt);
-        this.Portals.render(dt);
     }
 
     animate() {
@@ -162,131 +155,3 @@ export default class GLStage extends Component{
     }
 }
 
-
-function initPortals(props){
-
-    const portals = new PortalsMain(props)
-
-    portals.registerScene('scene-1', initScene1());
-    portals.registerScene('scene-2', initScene2());
-
-    // then create the portals
-    let portal1 = portals.createPortal(20, 20, 'scene-1');
-    portal1.position.set(-50, 10, 0);
-
-    let extraPortal = portals.createPortal(20, 20, 'scene-1');
-    extraPortal.position.set(50, 10, 0);
-
-    let portal2 = portals.createPortal(20, 20, 'scene-2');
-    portal2.position.set(0, 10, 0);
-    portal2.rotateY(Math.PI);
-
-    // then link the portals (portals don't have to be bi-directional)
-    portal1.setDestinationPortal(portal2);
-    portal2.setDestinationPortal(portal1);
-    extraPortal.setDestinationPortal(portal1);
-
-    // then set the starting scene
-    portals.setCurrentScene('scene-1');
-
-    // set the starting camera position
-    portals.setCameraPosition(0, 0, 60);
-
-    return portals;
-}
-
-
-
-
-
-function initScene1() {
-    const scene1 = new Scene();
-
-    // place some objects in the scene
-    let geometry, material, mesh, light;
-
-    // sky
-    geometry = new SphereGeometry(1000);
-    material = new MeshBasicMaterial({color: 0xaaaaaa, side: BackSide});
-    const mesh1 = new Mesh(geometry, material);
-    scene1.add(mesh1);
-
-    // a box in the center
-    geometry = new BoxGeometry(400, 25, 20);
-    material = new MeshBasicMaterial({color: 0xFFFFFF});
-    const mesh2 = new Mesh(geometry, material);
-    mesh2.position.set(0, 12.5, -10);
-    scene1.add(mesh2);
-
-    // something fancy to look at
-    geometry = new TorusKnotGeometry(10, 1, 64, 8, 3, 5);
-    material = new MeshBasicMaterial({color: 0x00FF00});
-    const mesh3 = new Mesh(geometry, material);
-    mesh3.position.set(0, 20, 75);
-    scene1.add(mesh3);
-
-    geometry = new TorusKnotGeometry(10, 1, 64, 8, 7, 11);
-    material = new MeshBasicMaterial({color: 0x00FF00});
-    const mesh4 = new Mesh(geometry, material);
-    mesh4.position.set(0, 20, -75);
-    scene1.add(mesh4);
-
-    // the floor
-    geometry = new PlaneBufferGeometry(400, 400);
-    material = new MeshBasicMaterial({color: 0x0000ff});
-    const mesh5 = new Mesh(geometry, material);
-    mesh5.rotation.x = Math.PI * 1.5;
-    scene1.add(mesh5);
-
-    // lights
-    light = new DirectionalLight(0x000000, 1);
-    light.position.set(1, 1, -1).normalize();
-    scene1.add(light);
-
-    light = new AmbientLight(0x101010);
-    scene1.add(light);
-    return scene1;
-}
-
-function initScene2() {
-    const scene2 = new Scene();
-
-    // place some objects in the scene
-    let geometry, material, mesh, light;
-
-    // sky
-    geometry = new SphereGeometry(1000);
-    material = new MeshBasicMaterial({color: 0xeeeeee, side: BackSide});
-    mesh = new Mesh(geometry, material);
-    scene2.add(mesh);
-
-    // a box in the center
-    geometry = new BoxGeometry(40, 25, 20);
-    material = new MeshBasicMaterial({color: 0xFFFFFF});
-    mesh = new Mesh(geometry, material);
-    mesh.position.set(0, 12.5, 10);
-    scene2.add(mesh);
-
-    // something fancy to look at
-    geometry = new TorusKnotGeometry(10, 4, 64, 8, 2, 3);
-    material = new MeshBasicMaterial({color: 0xFF0000});
-    mesh = new Mesh(geometry, material);
-    mesh.position.set(0, 20, -75);
-    scene2.add(mesh);
-
-    // the floor
-    geometry = new PlaneBufferGeometry(400, 400);
-    material = new MeshBasicMaterial({color: 0xff00ff});
-    mesh = new Mesh(geometry, material);
-    mesh.rotation.x = Math.PI * 1.5;
-    scene2.add(mesh);
-
-    // lights
-    light = new DirectionalLight(0x000000, 1);
-    light.position.set(1, 1, -1).normalize();
-    scene2.add(light);
-
-    light = new AmbientLight(0x101010);
-    scene2.add(light);
-    return scene2;
-}
